c37dd4eed13d0b1293c7ec2ff239a3b396bd901a
godwinmabeta <godwinmabeta@gmail.com>
2023-02-17 04:52:14 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,8 +13,8 @@
 
 int main(void)
 
-{
 
+{
 int n, lastd;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -32,6 +32,5 @@ else if (lastd < 6 && lastd != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 }
 return (0);
-
 }
     
