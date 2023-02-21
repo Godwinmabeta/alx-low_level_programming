ffcb3554e691f59f09bf07cedb98f38e6c660ffb
godwinmabeta <godwinmabeta@gmail.com>
2023-02-21 04:49:49 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-* print_last_digit - prints the last digit of a number
-* @n: The number to be treated
-* Return: value of the last digit of number
+*print_last_digit - prints the last digit of a number
+*@n:The number to be treated
+*Return:value of the last digit of number
 */
-
 int print_last_digit(int n)
-
 {
 int last;
 last = n % 10;
