a4d6f9da72f284106163f10f292120e12388c6cb
godwinmabeta <godwinmabeta@gmail.com>
2023-03-06 04:59:43 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -6,9 +6,9 @@
  * Return: Always 0.
  */
 
-int main(void) 
+int main(void)
 {
-	char *s0 = "Bob Dylan";  
+	char *s0 = "Bob Dylan"; 
 	char *s1 = "Robert Allen";     
 	printf("%s, %s\n", s0, s1);     
 	set_string(&s1, s0);
