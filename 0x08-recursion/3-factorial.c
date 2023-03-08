3dfe9b88e9ebf91d7d0e167d14b9bbf3b608b2fc
godwinmabeta <godwinmabeta@gmail.com>
2023-03-08 03:15:55 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,13 @@
 
 int factorial(int n)
 {
-	if (n == 0) /*Base condition*/
-	
-	return(1);
+if (n == 0) /*Base condition*/
 
-	else if (n < 0) /*Base condition*/
-	return (-1);
-	else
-	return (n * factorial(n - 1)); /*Recursive call*/
+return(1);
+
+else if (n < 0) /*Base condition*/
+return (-1);
+else
+return (n * factorial(n - 1)); /*Recursive call*/
 
 }
